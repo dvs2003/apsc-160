8654e3b20376785281132e025df7376cca4f5825
dvs <singhania.devesh@outlook.com>
2023-04-13 18:20:07 -0700

    pratik moment.

--- src/mainV1.cpp
+++ src/mainV1.cpp
@@ -1,18 +1,129 @@
 #include <Arduino.h>
 
 #define LEVELS 5
+#define LEDS 4
+#define LED_START 9
+#define BUTTON_START 4
 
 void setup(){
     true;
     /*Code*/
 }
+int levelCreation(int counter, int* level){
+    while (counter > 0)
+    {
+        counter--;
+        level[counter] = random(LEDS);
+        Serial.print(level[counter]);
+        Serial.print(" ");
+    }
+    return 1;
+}
+int showLeds(int level, int* array){
+    long int timer;
+    for (int i = 0; i <= level; i++)
+    {
+        for (int f = 1; f >= 0; f++)
+        {
+            digitalWrite(array[i] + LED_START, f);
+            timer = millis();
 
+            while (millis() - timer < 1000)
+            {
+                if (check_for_button() < LEDS)
+                {
+                    Serial.println("Lost!");
+                    return 0;
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    return 1;
+}
+
+int check_for_button(){
+    long timer;
+    for (int i = 0; i < LEDS; i++)
+    {
+        if (digitalRead(i + BUTTON_START))
+        {
+            while (digitalRead(i + BUTTON_START))
+            {
+                continue;
+            }
+            return i;
+        }
+        
+    }
+    
+    return LEDS;
+}
 void loop(){
+    int array[LEDS] = { 0 };
     int level = 0;
-
+    
+    levelCreation(LEVELS, array);
     while (level < LEVELS)
     {
-        continue;
+        if (!showLeds(level, array))
+        {
+            /* lost */
+            break;
+        }
+
+        else
+        {
+            int numUSERinputs = 0;
+            int timeout = millis();
+
+            while (numUSERinputs <= level)
+            {
+                if(millis() - timeout > 10000){
+                    //Lost
+                    level = LEVELS;
+                    numUSERinputs = LEVELS;
+                    break;
+                }
+
+                int button = check_for_button();
+
+                if (button != array[numUSERinputs])
+                {
+                    level = LEVELS;
+                    break;
+                    //LOST
+                }
+
+                else
+                {
+                    if (numUSERinputs == level)
+                    {
+                        //level change
+                        if (level == LEVELS - 1)
+                        {
+                            Serial.println("Won");
+                            //leds on and off
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        timeout = millis();
+                        numUSERinputs ++;
+                    }
+                    
+                }
+                
+                
+            }
+            
+        }
+        
+        
     }
     /*
     Rewrite the positioning of one of the logics
